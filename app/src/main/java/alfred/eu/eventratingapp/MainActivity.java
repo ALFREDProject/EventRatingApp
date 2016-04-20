252337804ec377ed7869956b32871587e1cdb328
juan.rodriguezp <juan.rodriguezp@atos.net>
2016-04-20 09:43:56 +0200

    Display event

--- app/src/main/java/alfred/eu/eventratingapp/MainActivity.java
+++ app/src/main/java/alfred/eu/eventratingapp/MainActivity.java
@@ -1,19 +1,34 @@
 package alfred.eu.eventratingapp;
 
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import alfred.eu.eventratingapp.actions.SubmitRatingForEvent;
 import eu.alfred.api.PersonalAssistantConnection;
 import eu.alfred.api.event.model.Event;
+import eu.alfred.api.event.recommendation.Eventrating;
 import eu.alfred.api.event.webservice.RecommendationManager;
+import eu.alfred.api.personalization.model.UserProfile;
+import eu.alfred.api.personalization.webservice.PersonalizationManager;
 import eu.alfred.ui.AppActivity;
 import eu.alfred.ui.CircleButton;
 
@@ -25,6 +40,8 @@ public class MainActivity extends AppActivity {
     private static final String SUBMIT_RATING = "SubmitRating";
 
     private RecommendationManager recommendationManager;
+    private PersonalizationManager personalizationManager;   //for UserProfile
+    private Event currentEvent;
 
     private int currentRate = 3;
     private ImageButton[] stars;
@@ -39,7 +56,18 @@ public class MainActivity extends AppActivity {
             @Override
             public void OnConnected() {
                 recommendationManager = new RecommendationManager(personalAssistant.getMessenger());
+                personalizationManager = new PersonalizationManager();
+
                 onNewIntent(getIntent());
+
+                //TODO
+                //How to get the event? Does it come from a notification?
+                // *********** Simulated ****************
+                currentEvent = new Event();
+                currentEvent.setTitle("Test event");
+                currentEvent.setStart_date(new Date());
+                displayEvent(currentEvent);
+                // **************************************
             }
 
             @Override
@@ -72,18 +100,26 @@ public class MainActivity extends AppActivity {
     public void performAction(String command, Map<String, String> map) {
         switch (command) {
             case (SUBMIT_RATING):
-                SubmitRatingForEvent cta = new SubmitRatingForEvent(this, cade,recommendationManager);
-                cta.performAction(command, map);
+                SubmitRatingForEvent srfe = new SubmitRatingForEvent(this, cade,recommendationManager);
+                srfe.performAction(command, map);
                 break;
             default:
                 break;
         }
     }
 
-    private void displayEvent(Event event) {
-        Log.d(LOGTAG, "Display event: " + event.toString());
 
+    private void displayEvent(Event event) {
+        Log.d(LOGTAG, "Display event: " + event.getTitle());
+
+        textViewTitle.setText(event.getTitle());
+        //TODO convert date in
+        // - today
+        // - tomorrow
+        // - 16 april...
+        textViewTime.setText(DateFormat.getDateTimeInstance().format(event.getStart_date()));
     }
+
 
     private void displayRate() {
         Log.d(LOGTAG, "Display rate: " + currentRate);
@@ -117,12 +153,43 @@ public class MainActivity extends AppActivity {
 
     public void onClickSubmit(View v) {
         Log.d(LOGTAG, "Submit button");
-        //TODO
-        Toast.makeText(MainActivity.this, "Submit!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Submit", Toast.LENGTH_SHORT).show();
+        Map<String, String> map = new HashMap<>();
+
+        //TODO get the correct userProfile
+        //UserProfile userProfile = personalizationManager.retrieveUserProfile(userId).get(0);
+        UserProfile userProfile = new UserProfile();
+
+        Eventrating eventrating = new Eventrating();
+        eventrating.setRating(currentRate);
+
+        map.put("userProfile", (new Gson()).toJson(userProfile));
+        map.put("eventrating", (new Gson()).toJson(eventrating));
+
+        performAction(SUBMIT_RATING, map);
     }
 
     public void onClickCancel(View v) {
         Log.d(LOGTAG, "Cancel button");
         finish();
     }
+
+
+    /**
+     * Receives the notification
+     * TODO: here it is supposed there is a JSON in the intent with the event
+     */
+    private void setBroadcast() {
+        //TODO set filter ?
+        IntentFilter filter = new IntentFilter();
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filter);
+    }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Type responseType = new TypeToken<Event>(){}.getType();
+            displayEvent((Event) (new Gson()).fromJson(intent.getStringExtra("event"), responseType) );
+        }
+    };
 }
--- app/src/main/java/alfred/eu/eventratingapp/actions/SubmitRatingForEvent.java
+++ app/src/main/java/alfred/eu/eventratingapp/actions/SubmitRatingForEvent.java
@@ -2,9 +2,14 @@ package alfred.eu.eventratingapp.actions;
 
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.Map;
 
 import alfred.eu.eventratingapp.MainActivity;
+import eu.alfred.api.event.model.Event;
 import eu.alfred.api.event.recommendation.Eventrating;
 import eu.alfred.api.event.webservice.RecommendationManager;
 import eu.alfred.api.personalization.model.UserProfile;
@@ -18,17 +23,22 @@ public class SubmitRatingForEvent implements ICadeCommand {
     MainActivity main;
     Cade cade;
     RecommendationManager recommendationManager;
+
     public SubmitRatingForEvent(MainActivity main, Cade cade, RecommendationManager recommendationManager) {
         this.main = main;
         this.cade = cade;
         this.recommendationManager = recommendationManager;
     }
+
     @Override
     public void performAction(String s, Map<String, String> map) {
-        Log.i("EventRating-Log", map.get("SubmitRating"));
-        UserProfile hereIsSomethingMissing = null;
-        Eventrating hereIsSomethingMissing2 = null;
-        recommendationManager.createEventRecommendationAnswer(hereIsSomethingMissing2, hereIsSomethingMissing);
-        throw new UnsupportedOperationException("Here is some logic missing, but it should return data ");
+        //Log.i("EventRating-Log", map.get("SubmitRating"));
+        //Receives JSON parameters to create recommendations answer
+        Type responseType1 = new TypeToken<UserProfile>(){}.getType();
+        UserProfile userProfile = (new Gson()).fromJson(map.get("userProfile"), responseType1);
+        Type responseType2 = new TypeToken<Eventrating>(){}.getType();
+        Eventrating eventrating = (new Gson()).fromJson(map.get("eventrating"), responseType2);
+
+        recommendationManager.createEventRecommendationAnswer(eventrating, userProfile);
     }
 }
